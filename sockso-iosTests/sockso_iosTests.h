f5059dca08becedc5683ab0efe692b9b1bb58535
rod <rod@pu-gh.com>
2011-06-09 19:54:08 +0100

    Initial Commit

---
+++ sockso-iosTests/sockso_iosTests.h
@@ -0,0 +1,17 @@
+//
+//  sockso_iosTests.h
+//  sockso-iosTests
+//
+//  Created by Rod on 09/06/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface sockso_iosTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ sockso-iosTests/sockso_iosTests.m
@@ -0,0 +1,33 @@
+//
+//  sockso_iosTests.m
+//  sockso-iosTests
+//
+//  Created by Rod on 09/06/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "sockso_iosTests.h"
+
+
+@implementation sockso_iosTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in sockso-iosTests");
+}
+
+@end
