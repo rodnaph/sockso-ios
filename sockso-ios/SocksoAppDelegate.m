1e7d18572c3c6b70b98da307beaa660898744ed6
rod <rod@pu-gh.com>
2011-09-20 00:33:29 +0100

    cleaned up init a bit

--- sockso-ios/SocksoAppDelegate.m
+++ sockso-ios/SocksoAppDelegate.m
@@ -3,30 +3,47 @@
 #import "ConnectViewController.h"
 #import "SocksoModule.h"
 
+@interface SocksoAppDelegate (Private)
+
+- (void)initInjection;
+- (void)initInterface;
+
+@end
+
 @implementation SocksoAppDelegate
 
 @synthesize window, navigationController;
 
 - (void) applicationDidFinishLaunching:(UIApplication *)application {
 
+    [self initInjection];
+    [self initInterface];
+    
+}
+
+- (void)initInjection {
+    
     SocksoModule *socksoModule = [[[SocksoModule alloc] init] autorelease];
     JSObjectionInjector *injector = [JSObjection createInjector:socksoModule];
 
     [JSObjection setGlobalInjector:injector];
     
-    ConnectViewController *aView = [[injector getObject:[ConnectViewController class]]
-                                    initWithNibName:@"ConnectView" bundle:nil];
+    [socksoModule release];
+    
+}
+
+- (void)initInterface {
+    
+    JSObjectionInjector *injector = [JSObjection globalInjector];
+    ConnectViewController *controller = [[injector getObject:[ConnectViewController class]]
+                                         initWithNibName:@"ConnectView" bundle:nil];
     
     UINavigationController *navController = [[UINavigationController alloc]
-                                             initWithRootViewController:aView];
-    
-    NSLog( @"Create navigation controller" );
+                                             initWithRootViewController:controller];
     
     self.navigationController = navController;
     [navController release];
-    [aView release];
-    
-    NSLog( @"Make window visible" );
+    [controller release];
     
     [self.window addSubview:navigationController.view];
     [self.window makeKeyAndVisible];
